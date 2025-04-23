518a6e681b320527689227f014bbece13745601a
ariostus <ludovicopicchio@icloud.com>
2025-04-24 01:10:41 +0200

    text max chars

--- wishlist/forms.py
+++ wishlist/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class WishUpdate(forms.Form):
-    name = forms.CharField(max_length=11, required=False)
+    name = forms.CharField(max_length=20, required=False)
     notes = forms.CharField(max_length=400, widget=forms.Textarea, required=False)
     link = forms.URLField(required=False)
     status = forms.BooleanField(required=False, initial=False)
@@ -14,5 +14,5 @@ class DeleteForm(forms.Form):
 
 
 class ListUpdate(forms.Form):
-    title = forms.CharField(max_length=100, required=False)
+    title = forms.CharField(max_length=30, required=False)
     description = forms.CharField(max_length=1000, widget=forms.Textarea, required=False)
